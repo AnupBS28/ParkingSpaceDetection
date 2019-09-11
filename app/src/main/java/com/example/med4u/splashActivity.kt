224ea99569651885d6fef3af7205c2c38d639f67
AnupBS28 <52864920+AnupBS28@users.noreply.github.com>
2019-09-11 22:07:58 +0530

    Initial commit

---
+++ app/src/main/java/com/example/med4u/splashActivity.kt
@@ -0,0 +1,12 @@
+package com.example.med4u
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class splashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
